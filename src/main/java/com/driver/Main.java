8f9578c3d196cb500501cbfc7abffdb54f920df1
shubham chambhare <shubham961120@gmail.com>
2023-11-03 17:40:45 +0530

    first push

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,14 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+        Boat b1 = new Boat("farrari",8);
+        System.out.print(b1.getVehicleName());
     }
 }
